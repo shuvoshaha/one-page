1b50c49f8ceb77dd4b37a4fcad4fffe8e1a3305e
shuvoshaha <shaha460@gmail.com>
2020-11-30 13:04:58 +0600

    One page website

---
+++ js/custom..js
@@ -0,0 +1,8 @@
+var title = document.getElementById("overlay");
+title.addEventListener("mouseover", function() {
+  title.innerHTML = "Digital";
+});
+
+title.addEventListener("mouseout", function() {
+  title.innerHTML = "Spotlight.";
+});
